bb11b5973fe8f48a92c9b1c959bb93d695389209
Jonhtan Mota <jonathanmotareis@gmail.com>
2020-11-11 13:09:45 -0300

    comentado o logo do vue e ajustado espaçamento do topo da tela.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <prototipo/>
   </div>
 </template>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 12px;
 }
 </style>
